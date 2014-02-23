306034dc13b85c3d9a57db14c883a602e2bc1ada
Vladimir <abramichevvg@sibedge.com>
2014-02-24 00:41:03 +0700

    #4 - fdfsd sd f-sdf sdfsd

---
+++ GoWindows/GoWindows/ViewModels/ViewModel.cs
@@ -0,0 +1,30 @@
+lfg'fgk df
+gdfg
+ df
+ g
+  d  
+  
+   
+    
+	 fg
+	 df
+	  
+	  dfg
+	   
+	   fg
+	   d f
+	   d
+	   
+	   g 
+	   df
+	    
+		fg
+		
+		fg
+		df 
+		df
+		 
+		 dfg
+		 f
+		 d
+		 dfgd
